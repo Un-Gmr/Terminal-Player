7712e03a8ccff75f4385d0ec6009d0c11e65e17e
Un-Gmr <9999tablet9999+github@gmail.com>
2026-02-05 17:37:07 +0100

    Initial commit

---
+++ play.sh
@@ -0,0 +1,161 @@
+#!/usr/bin/env bash
+
+LOOP=false
+SEARCH=""
+LYRIC_OFFSET=1
+
+for arg in "$@"; do
+    if [ "$arg" = "-l" ]; then
+        LOOP=true
+    else
+        SEARCH="$SEARCH $arg"
+    fi
+done
+
+SEARCH=$(echo "$SEARCH" | sed 's/^ *//;s/ *$//')
+
+if [ -z "$SEARCH" ]; then
+    echo "Usage: play <search terms> [-l]" >&2
+    exit 1
+fi
+
+METADATA=$(yt-dlp "ytsearch1:$SEARCH" -q --no-warnings --no-download --print-json)
+
+URL=$(echo "$METADATA" | jq -r '.webpage_url')
+TITLE=$(echo "$METADATA" | jq -r '.title // "N/A"')
+ARTIST=$(echo "$METADATA" | jq -r '.artist // "N/A"')
+ALBUM=$(echo "$METADATA" | jq -r '.album // "N/A"')
+DATE=$(echo "$METADATA" | jq -r '.upload_date // "N/A"')
+UPLOADER=$(echo "$METADATA" | jq -r '.uploader // "N/A"')
+THUMB=$(echo "$METADATA" | jq -r '.thumbnail // empty')
+
+TERM_WIDTH=$(tput cols)
+COVER_WIDTH=$((TERM_WIDTH / 2))
+
+COVER_FILE=""
+LYRICS_FILE=$(mktemp)
+trap 'rm -f "$COVER_FILE" "$LYRICS_FILE"; stty ixon echo; exit' INT
+
+if [ -n "$THUMB" ]; then
+    COVER_FILE=$(mktemp /tmp/cover.XXXXXX.jpg)
+    curl -fsSL "$THUMB" -o "$COVER_FILE"
+    if [ -f "$COVER_FILE" ]; then
+        mapfile -t COVER_LINES < <(jp2a --colors --fill --width=$COVER_WIDTH "$COVER_FILE")
+        magick "$COVER_FILE" -resize 128x128^ -gravity center -extent 128x128 "$COVER_FILE.cropped.png"
+        notify-send -u critical -t 5000 -i "$COVER_FILE.cropped.png" "Terminal-Player" "Playing: $TITLE-$ARTIST"
+    fi
+fi
+
+INFO_LINES=(
+    "Title:   $TITLE"
+    "Artist:  $ARTIST"
+    "Album:   $ALBUM"
+    "Date:    $DATE"
+    "Channel: $UPLOADER"
+    "URL:     $URL"
+    "Percent: 00:00:00 / 00:00:00 (0%)"
+    "Volume:  100%"
+)
+
+clear
+
+max_lines=${#COVER_LINES[@]}
+if [ ${#INFO_LINES[@]} -gt $max_lines ]; then
+    max_lines=${#INFO_LINES[@]}
+fi
+
+for ((i=0; i<max_lines; i++)); do
+    printf "%-${COVER_WIDTH}s  %s\n" \
+        "${COVER_LINES[i]}" \
+        "${INFO_LINES[i]}"
+done
+
+PERCENT_LINE=$(( ${#INFO_LINES[@]} - 2 ))
+VOLUME_LINE=$((PERCENT_LINE + 1))
+
+update_percent() {
+    local percent="$1"
+    tput cup $PERCENT_LINE $COVER_WIDTH
+    printf "  Percent: %s  " "$percent"
+    tput cup $((max_lines+1)) 0
+}
+
+update_volume() {
+    local vol
+    vol=$(echo '{ "command": ["get_property", "volume"] }' | socat - /tmp/mpvsocket 2>/dev/null | jq -r '.data // "N/A"')
+    vol=${vol%.*}
+    tput cup $VOLUME_LINE $COVER_WIDTH
+    printf "  Volume:  %s%%  " "$vol"
+    tput cup $((max_lines+2)) 0
+}
+
+python -m syncedlyrics -o="$LYRICS_FILE" "[$TITLE] [$ARTIST]" >>/dev/null 2>/dev/null
+
+LYRIC_HEIGHT=24
+
+show_lyrics() {
+    local time="$1"
+    local mm ss cur last
+    mm=${time:3:2}
+    ss=${time:6:2}
+    cur=$((10#$mm*60 + 10#$ss + LYRIC_OFFSET))
+    [ "$cur" -lt 0 ] && cur=0
+
+    last=$(awk -F']' -v t="$cur" '
+        {
+            gsub(/^\[/,"",$1)
+            split($1,ts,":")
+            cur=int(ts[1]*60 + ts[2])
+            if(cur<=t) last=$2
+        }
+        END{
+            gsub(/\n/,"",last)
+            print last
+        }
+    ' "$LYRICS_FILE")
+
+    local start_line=$((max_lines + 1))
+
+    for ((i=0;i<LYRIC_HEIGHT;i++)); do
+        tput cup $((start_line+i)) 0
+        printf "%-${TERM_WIDTH}s" ""
+    done
+
+    tput cup $start_line 0
+    figlet -f small -w $TERM_WIDTH "$last"
+}
+
+MPV_ARGS="--no-video --cache=no --input-ipc-server=/tmp/mpvsocket"
+$LOOP && MPV_ARGS="$MPV_ARGS --loop"
+
+stty_orig=$(stty -g)
+
+PERCENT="0%"
+mpv $MPV_ARGS "$URL" 2>&1 | while IFS= read -r line; do
+    if [[ "$line" == *"A:"* ]]; then
+        PERCENT="${line#*A:}"
+        PERCENT="${PERCENT#"${PERCENT%%[![:space:]]*}"}"
+        TIME="${PERCENT:0:8}"
+        update_percent "$PERCENT"
+        update_volume
+        show_lyrics "$TIME"
+    fi
+
+    read -rsn1 -t 0.01 key < /dev/tty
+        if [ "$key" = $'s' ]; then 
+            pkill -P $$ mpv 2>/dev/null 
+            break
+        elif [ "$key" = $'=' ]; then
+            echo '{ "command": ["add", "volume", 5] }' | socat - /tmp/mpvsocket
+        elif [ "$key" = $'+' ]; then
+            echo '{ "command": ["add", "volume", 5] }' | socat - /tmp/mpvsocket
+        elif [ "$key" = $'-' ]; then
+            echo '{ "command": ["add", "volume", -5] }' | socat - /tmp/mpvsocket
+        elif [ "$key" = " " ]; then
+            echo '{ "command": ["cycle", "pause"] }' | socat - /tmp/mpvsocket
+        fi
+done
+
+stty "$stty_orig"
+[ -n "$COVER_FILE" ] && rm -f "$COVER_FILE"
+rm -f "$LYRICS_FILE"
---
+++ playlist.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+[ -t 0 ] && stty -ixon
+
+filename=$1
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+PLAYLIST_DIR="$XDG_DATA_HOME/play/playlists"
+mkdir -p $PLAYLIST_DIR >> /dev/zero
+filepath="$PLAYLIST_DIR/${filename}.txt"
+
+if [ $# -lt 1 ]; then
+    echo "Usage: playlist filename [-l] [-s]" >&2
+    echo "Playlist dir: ~/.local/share/play/playlists" >&2
+    exit 1
+fi
+
+filename=$1
+shift
+
+loop=false
+shuffle=false
+
+for arg in "$@"; do
+    case $arg in
+        -l) loop=true ;;
+        -s) shuffle=true ;;
+        *) echo "Unknown option: $arg" >&2; exit 1 ;;
+    esac
+done
+
+if [ ! -f "$filepath" ]; then
+    echo "File not found: $filepath" >&2
+    exit 1
+fi
+
+mapfile -t original_lines < "$filepath"
+
+quit=false
+
+play_playlist() {
+    local lines=("${@}")
+    for line in "${lines[@]}"; do
+        if $quit; then
+            break
+        fi
+        play "$line" &
+        pid=$!
+        
+        while kill -0 $pid 2>/dev/null; do
+            read -rsn1 -t 0.1 key < /dev/tty
+            if [[ $key == "q" ]]; then
+                pkill mpv
+                sleep .1
+                kill $pid 2>/dev/null
+                quit=true
+                break
+            fi
+        done
+    done
+}
+
+if $loop; then
+    while ! $quit; do
+        if $shuffle; then
+            mapfile -t shuffled_lines < <(printf '%s\n' "${original_lines[@]}" | shuf)
+            play_playlist "${shuffled_lines[@]}"
+        else
+            play_playlist "${original_lines[@]}"
+        fi
+    done
+else
+    if $shuffle; then
+        mapfile -t shuffled_lines < <(printf '%s\n' "${original_lines[@]}" | shuf)
+        play_playlist "${shuffled_lines[@]}"
+    else
+        play_playlist "${original_lines[@]}"
+    fi
+fi
